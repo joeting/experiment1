cda7afd4d06d0e7d8cac7e2772d0927d336a1388
Joe Ting <joe.ting@disney.com>
2014-03-29 17:29:06 -0700

    fix indents

--- crawler/src/main/java/com/loopnet/crawler/Runner.java
+++ crawler/src/main/java/com/loopnet/crawler/Runner.java
@@ -6,9 +6,10 @@ import com.loopnet.crawler.provider.BizBuySell;
  * Runs the parser
  *
  */
-public class Runner 
-{
-	public static void main(String argv[]) throws Exception {
-    	new BizBuySell().loopListingCategory("http://www.bizbuysell.com", "gas-stations-for-sale");
-    }
+public class Runner {
+  
+  public static void main(String argv[]) throws Exception {
+	new BizBuySell().loopListingCategory("http://www.bizbuysell.com", "gas-stations-for-sale");
+  }
+  
 }
--- crawler/src/main/java/com/loopnet/crawler/dao/ListingDao.java
+++ crawler/src/main/java/com/loopnet/crawler/dao/ListingDao.java
@@ -2,32 +2,31 @@ package com.loopnet.crawler.dao;
 
 import java.net.UnknownHostException;
 
-
 import com.github.jmkgreen.morphia.Datastore;
 import com.github.jmkgreen.morphia.Morphia;
 import com.loopnet.crawler.data.Listing;
 import com.mongodb.Mongo;
 
 public class ListingDao {
-	
-	public static final String DB_NAME = "marketplace";
-	private Datastore datastore;
-	
-	private ListingDao(Datastore datastore) {
-		this.datastore = datastore;
-	}
-	
-	public static ListingDao listingDao() throws UnknownHostException {
-		Mongo mongo = new Mongo("localhost", 27017);
-		Datastore ds = new Morphia()
-    		.map(Listing.class)
-    		.createDatastore(mongo, DB_NAME);
-		ds.ensureIndexes();
-		return new ListingDao(ds);
-	}
-	
-	public void insert(Listing listing) {
-		datastore.save(listing);
-	}
-	
+
+  public static final String DB_NAME = "marketplace";
+  private Datastore datastore;
+
+  private ListingDao(Datastore datastore) {
+	this.datastore = datastore;
+  }
+
+  public static ListingDao listingDao() throws UnknownHostException {
+	Mongo mongo = new Mongo("localhost", 27017);
+	Datastore ds = new Morphia()
+		.map(Listing.class)
+		.createDatastore(mongo, DB_NAME);
+	ds.ensureIndexes();
+	return new ListingDao(ds);
+  }
+
+  public void insert(Listing listing) {
+	datastore.save(listing);
+  }
+
 }
